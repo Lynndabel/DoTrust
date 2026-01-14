35d49751e50463d795e2271e068c9d3cdd389737
Ebele Lynda Okolo <okolo.ebelelynda@gmail.com>
2026-01-14 12:20:07 +0100

    Revert "Lynn"

--- frontend/client/src/rainbowKitConfig.jsx
+++ frontend/client/src/rainbowKitConfig.jsx
@@ -1,15 +1,18 @@
 "use client";
 
 import { getDefaultConfig } from "@rainbow-me/rainbowkit";
-import { polygonAmoy } from "wagmi/chains";
+import { polygon} from "wagmi/chains";
 import { http } from "wagmi";
 
 export default getDefaultConfig({
-  appName: "DoTrust",
-  projectId: import.meta.env.VITE_WALLETCONNECT_PROJECT_ID || "YOUR_WALLETCONNECT_PROJECT_ID",
-  chains: [polygonAmoy],
-  transports: {
-    [polygonAmoy.id]: http(import.meta.env.VITE_RPC_URL || "https://rpc-amoy.polygon.technology"),
-  },
+  appName: "Cross-Credit Lending",
+  projectId: import.meta.env.VITE_WALLETCONNECT_PROJECT_ID,
+  chains: [polygon],
+  // transports: {
+  //   [polygonAmoy.id]: [
+  //     http(import.meta.env.VITE_ALCHEMY_RPC_URL),
+  //     http(import.meta.env.VITE_INFURA_RPC_URL),
+  //   ],
+  // },
   ssr: false,
 });
